9d61f19ad740cf579685513e8715f3a834e055fe
salima <salima.hafirassou@hotmail.com>
2021-04-10 12:56:48 +0200

    gitignore

---
+++ src/test/java/com/anas/projectTtestAnas/BiTest.java
@@ -0,0 +1,14 @@
+package com.anas.projectTtestAnas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BiTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
